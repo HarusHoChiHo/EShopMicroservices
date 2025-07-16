256c8719b3dadf6ea968aea61c3385e1149637f7
harusho <harusho@pm.me>
2025-07-15 22:55:43 -0400

    Finished Basket.API

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,10 @@
+﻿global using BuildingBlocks.CQRS;
+global using Basket.API.Models;
+global using Carter;
+global using FluentValidation;
+global using Mapster;
+global using MediatR;
+global using BuildingBlocks.Behaviors;
+global using Marten;
+global using BuildingBlocks.Exceptions;
+global using Basket.API.Data;
--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -9,3 +9,5 @@ global using FluentValidation;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions.Handler;
 global using CatalogAPI.Data;
+global using CatalogAPI.Data.Models;
+global using Marten.Pagination;
