377d93a3c90e63e11ff5b297aa6575ffb5c05bd4
harusho <harusho@pm.me>
2025-07-19 18:58:18 -0400

    Adding Async Event handler

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,6 @@
 using Basket.API.Data;
 using BuildingBlocks.Exceptions.Handler;
+using BuildingBlocks.Messaging.MassTransit;
 using DiscountGrpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
@@ -52,6 +53,9 @@ builder.Services
                                                };
                                                return handler;
                                            });
+
+//Async Communication Services
+builder.Services.AddMessageBroker(builder.Configuration);
 
 //Cross-cutting services
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
